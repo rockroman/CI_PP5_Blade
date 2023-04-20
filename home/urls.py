1918131fa17df4c5229cfb4457f0a5e2c082db34
Roman Rakic <rock.rakic@gmail.com>
2023-04-20 22:37:10 +0100

    add home app that displays landing page , create, url, views and wire up templates

---
+++ home/urls.py
@@ -0,0 +1,16 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.contrib import admin
+from django.urls import path
+
+# Internal:
+from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+app_name = 'home'
+urlpatterns = [
+    path('', views.home, name='home'),
+
+]
---
+++ home/views.py
@@ -0,0 +1,18 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.shortcuts import render
+
+# Internal:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def home(request):
+    """
+    view to render the landing page
+    """
+    template = 'home/index.html'
+    # context = {}
+    return render(request, template)
+
+
