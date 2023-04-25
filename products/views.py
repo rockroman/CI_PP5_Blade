ca3b72a16d155c8237602841a51c17d1086d5a62
Roman Rakic <rock.rakic@gmail.com>
2023-04-25 20:51:49 +0100

    feat: add sorting products functionality on a landing page

--- products/views.py
+++ products/views.py
@@ -4,6 +4,8 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 
 # Internal:
 from .models import Product, Category
@@ -16,8 +18,22 @@ def store_products(request):
     template = 'products/store_products.html'
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in  request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -32,11 +48,13 @@ def store_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'sorting': sorting,
     }
 
     return render(request,template, context)
