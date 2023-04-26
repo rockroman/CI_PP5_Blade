9e1f86dcad78044c70c8651fd8797e2e9a7dba89
Roman Rakic <rock.rakic@gmail.com>
2023-04-26 17:11:25 +0100

    feat:add basic product filtering option on product pages

--- products/views.py
+++ products/views.py
@@ -28,6 +28,8 @@ def store_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey =='category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
