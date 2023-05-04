6c25b17c39fbf5f88330702c1e9db75b62d7ab5f
Roman Rakic <rock.rakic@gmail.com>
2023-05-04 13:40:22 +0100

    feat: added order form and signals

---
+++ checkout/urls.py
@@ -0,0 +1,18 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.contrib import admin
+from django.urls import path
+
+# Internal:
+from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+app_name = 'checkout'
+
+urlpatterns = [
+    path('')
+
+
+]
