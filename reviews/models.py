e7458f77204f165c5a2d1248373c7fc05d032382
Roman Rakic <rock.rakic@gmail.com>
2023-05-18 17:51:50 +0100

    feat: create review app and basic model for reviews of store products

---
+++ reviews/models.py
@@ -0,0 +1,26 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.db import models
+from django.contrib.auth.models import User
+
+# Internal:
+from products.models import Product
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Review(models.Model):
+    """
+    class that is handling user reviews of a store
+    product
+    """
+    author = models.ForeignKey(User,
+                               on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1024)
+    time_posted = models.TimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Review by {self.author.username} for {self.product.name}'
---
+++ reviews/urls.py
@@ -0,0 +1,19 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.contrib import admin
+from django.urls import path
+
+# Internal:
+from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+app_name = 'reviews'
+
+urlpatterns = [
+    path('', ),
+
+
+
+]
