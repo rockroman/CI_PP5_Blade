5262ba47133cfae1a2a3f91e9603611c39d25e5b
Roman Rakic <rock.rakic@gmail.com>
2025-05-13 10:00:32 +0100

    adding validation for price and blade length

--- products/forms.py
+++ products/forms.py
@@ -8,6 +8,7 @@ from .models import Product, Category
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+
 class ProductForm(forms.ModelForm):
     """
     form for adding and editing products in
@@ -18,9 +19,20 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+    def clean(self):
+        cleaned_data = super().clean()
+        price = cleaned_data.get("price")
+        bladelength = cleaned_data.get("bladelength")
+
+        if price is not None and price < 0:
+            self.add_error('price', 'Price must be greater than 0')
+        if bladelength is not None and bladelength < 0:
+            self.add_error('bladelength', 'Blade length must be greater than 0')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
+
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
